5799ab87571a9d82021b79d28f67f29619d17227
al221910432 <90278124+al221910432@users.noreply.github.com>
2023-11-18 17:55:09 -0600

    Update ProductRepository.java

--- src/main/java/com/example/oxxo/repository/ProductRepository.java
+++ src/main/java/com/example/oxxo/repository/ProductRepository.java
@@ -11,22 +11,22 @@ import com.example.oxxo.entity.Product;
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
-    @Query(value = "Select * from Product order by id desc limit :number",nativeQuery = true)
+    @Query(value = "Select * from product order by id desc limit :number",nativeQuery = true)
     List<Product> getListNewest(int number);
 
-    @Query(value = "Select * from Product order by price limit 8 ",nativeQuery = true)
+    @Query(value = "Select * from product order by price limit 8 ",nativeQuery = true)
     List<Product> getListByPrice();
 
-    @Query(value ="Select * from Product where category_id = :id order by rand() limit 4",nativeQuery = true)
+    @Query(value ="Select * from product where category_id = :id order by rand() limit 4",nativeQuery = true)
     List<Product> findRelatedProduct(long id);
 
-    @Query(value ="Select * from Product where category_id = :id",nativeQuery = true)
+    @Query(value ="Select * from product where category_id = :id",nativeQuery = true)
     List<Product> getListProductByCategory(long id);
 
-    @Query(value = "Select * from Product where category_id = :id and price between :min and :max",nativeQuery = true)
+    @Query(value = "Select * from product where category_id = :id and price between :min and :max",nativeQuery = true)
     List<Product> getListProductByPriceRange(long id,int min,int max);
 
-    @Query(value= "Select p from Product p where p.name like %:keyword% order by id desc")
+    @Query(value= "Select p from product p where p.name like %:keyword% order by id desc")
     List<Product> searchProduct(String keyword);
 
 }
