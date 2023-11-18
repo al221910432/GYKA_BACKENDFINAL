a7d40918c1c78c7fbf1d24c7d4261e6b3a489ff6
al221910432 <90278124+al221910432@users.noreply.github.com>
2023-11-18 17:41:08 -0600

    Update OrderService.java

--- src/main/java/com/example/oxxo/service/OrderService.java
+++ src/main/java/com/example/oxxo/service/OrderService.java
@@ -2,22 +2,19 @@ package com.example.oxxo.service;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.oxxo.entity.Order;
 import com.example.oxxo.model.request.CreateOrderRequest;
 import com.example.oxxo.model.request.UpdateOrderRequest;
 
-public interface OrderService extends JpaRepository<Order, Long> {
+public interface OrderService {
     
     void placeOrder(CreateOrderRequest request);
 
     List<Order> getList();
     
-    List<Order> findByUserUsername(String username);
+    List<Order> getOrderByUser(String username);
 
     void updateOrder(Long orderId, UpdateOrderRequest request);
 
 
 }
-
